031413addfbd508c9130ee365841d61fc2538b4a
leonardomaia <leuow95@gmail.com>
2024-06-07 22:50:31 -0400

    exercicio basico loops e fatorial

---
+++ src/Exercicios/Loops.java
@@ -0,0 +1,37 @@
+package Exercicios;
+
+public class Loops {
+    public static void main(String[] args) {
+//        de1A150();
+//        de1A1000();
+//        multiply3();
+    factorial(20);
+
+    }
+
+    public static void de1A150(){
+        for(int i = 150; i <= 300; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void de1A1000(){
+        for(int i = 1; i <= 1000; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void multiply3(){
+        for(int i =1; i <= 100; i++){
+            if(i%3==0) System.out.println(i);
+        }
+    }
+
+    public static void factorial(int value){
+        long fatorial = value;
+        for (; value > 1; value--){
+            fatorial *= value-1;
+        }
+        System.out.println("fatorial:" + fatorial);
+    }
+}
